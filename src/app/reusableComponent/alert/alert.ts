34a6514d8597320005ccaba8fa31714d13d20d6b
Abdullah Bahar <abdullahbahar007@gmail.com>
2025-12-09 19:22:39 +0300

    reusableComponent altında terkar tekra kullanılması muhtemel bileşenler oluşturulup iligli componentler içerisinde kullanıldı. reusableComponent alert ve tab componentleri açıldı.

---
+++ src/app/reusableComponent/alert/alert.ts
@@ -0,0 +1,19 @@
+import { NgClass } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-alert',
+	imports: [NgClass],
+	templateUrl: './alert.html',
+	styleUrl: './alert.css',
+})
+
+export class Alert implements OnInit {
+	@Input() alertType: string = '';
+	@Input() alertMessage: string = '';
+
+	// className: string = '';
+	ngOnInit(): void {
+		// this.className =  'alert-' + this.alertType;
+	}
+}
